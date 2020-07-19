8679479cef4207dac29f3a395c31938d336ed150
myzyryzm <ryanyoung3e@yahoo.com>
2020-07-18 22:02:23 -0700

    boto3 to upload to s3

--- test.py
+++ test.py
@@ -2,4 +2,12 @@ import boto3
 import os
 import sys
 
-print(sys.argv)
+s3 = boto3.client('s3')
+
+video = sys.argv[1]
+try:
+    with open(video, 'rb') as f:
+        s3.put_object(Body=f, Bucket='evt-lect-dev-test', Key='video.mp4')
+except:
+    print('error')
+
